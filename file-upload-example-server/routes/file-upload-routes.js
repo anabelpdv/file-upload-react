fb010c6f6adfc7d8f95cbd1724a5a82820ee5630
Anabel Perez <anabelpdv@gmail.com>
2019-10-25 19:43:00 -0400

    Remove service functions

--- file-upload-example-server/routes/file-upload-routes.js
+++ file-upload-example-server/routes/file-upload-routes.js
@@ -5,11 +5,12 @@ const router = express.Router();
 const uploader = require('../configs/cloudinary-setup');
 
 router.post('/uploads', uploader.single('imageUrl'), (req,res,body)=>{
+
   if(!req.file){
     next(new Error('No file uploaded'))
     return;
   }
-  res.json({secure_url: req.file.secure_url});
+    res.json({secure_url: req.file.secure_url});
 })
 
 module.exports = router;
